a575b74f1d52e2816c689b21a59471e738d90502
zhantia1 <tonyzedz@hotmail.com>
2018-09-17 16:29:12 -0700

    Completed JSON.stringify - almost completed getElementsbyClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,20 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+
+var getElementsByClassName = function(className, element = document.body) {
   // your code here
   var results = [];
-  var body = this.document.body.childNodes;
-  console.log(body);
+  var children = element.childNodes;
   
-  for(var i = 0; i < body.length; i++) {
-    console.log(body[i]);
-    if(body[i].classList.includes(className)) {
-      results.push(body[i]);
-      console.log(results);
-    } else if(body[i].hasChildNodes) {
-      results.push(body[i].getElementsByClassName(className));
+  
+  for(var i = 0; i < element.classList.length; i++) {
+    if(element.classList[i] === className) {
+      results.push(element);
     }
   }
+
+  for(var i = 0; i < children.length; i++) {
+  
+    if(children[i].classList !== undefined) {
+      for(var j = 0; j < children[i].classList.length; j++) {
+        if(children[i].classList[j] === className) {
+          results.push(children[i]);
+        }        
+      }
+      
+      if(children[i].hasChildNodes()) {
+        results.concat(getElementsByClassName(className, children[i]));
+      }
+    }
+  }
+  
+  console.log(results);
   return results;
+  
 };
