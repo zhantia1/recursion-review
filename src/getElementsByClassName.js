40f844a9a12d45012a94b9303b9ddf1a5694d465
zhantia1 <tonyzedz@hotmail.com>
2018-09-17 20:43:46 -0700

    Completed Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,31 +8,44 @@
 var getElementsByClassName = function(className, element = document.body) {
   // your code here
   var results = [];
-  var children = element.childNodes;
-  
+  var children = element.childNodes; //[text, div1, text, div2]
   
   for(var i = 0; i < element.classList.length; i++) {
     if(element.classList[i] === className) {
-      results.push(element);
+      results.push(element); // results = [body.classTarget]
     }
   }
+  
+  for(var i = 0; i < children.length; i++) {
+    
+    if(children[i].classList !== undefined) {
+      
+      if(element.hasChildNodes()) {
+        results = results.concat(getElementsByClassName(className, children[i]));
+      }      
+    }
+  }
+  
 
+  /**
   for(var i = 0; i < children.length; i++) {
   
     if(children[i].classList !== undefined) {
+      
       for(var j = 0; j < children[i].classList.length; j++) {
         if(children[i].classList[j] === className) {
           results.push(children[i]);
         }        
       }
       
-      if(children[i].hasChildNodes()) {
+      if(element.hasChildNodes()) {
         results.concat(getElementsByClassName(className, children[i]));
       }
     }
-  }
+    
+  }*/
   
-  console.log(results);
+  
   return results;
   
 };
